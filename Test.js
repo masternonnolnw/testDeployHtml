b861a398a11c42c42594c3d440d7f034bbcc36ff
MaStEr <masternonnolnw@gmail.com>
2022-03-30 22:56:16 +0700

    .

---
+++ Test.js
@@ -0,0 +1,8 @@
+
+export default function Test(){
+      return (<>
+      {/* test
+      <iframe src="/src/test/TestDocument.html" width="1000px" height="1000px"></iframe> */}
+      </>);
+}
+<link rel="stylesheet" href="base.min.css"/>
